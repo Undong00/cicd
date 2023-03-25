8c2fb3b3081b09a2f86399305c773c372d260647
Undong00 <ehddjs85246@gmail.com>
2023-03-25 22:09:21 +0900

    .

--- application.py
+++ application.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 application = Flask(__name__)
 
 
 @application.route("/")
 def hello():
-    return render_template("index.html")
-
-
+    return "Hello World!"
 # run the app.
 if __name__ == "__main__":
     application.run()
